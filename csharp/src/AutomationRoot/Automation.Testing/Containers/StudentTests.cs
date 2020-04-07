e77444e4a989fc6e5f514793980d51905e2a9485
patutinskiy <patutinskiy-feod@mail.ru>
2020-04-07 13:22:23 +0300

    Completed 9.39 lesson Added test parameters

--- csharp/src/AutomationRoot/Automation.Testing/Containers/StudentTests.cs
+++ csharp/src/AutomationRoot/Automation.Testing/Containers/StudentTests.cs
@@ -1,16 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
 using Automation.Testing.Cases;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 
 namespace Automation.Testing.Containers
 {
     [TestClass]
     public class StudentTests
     {
-        [TestMethod]
-        public void SearchStudentUiTest()
+        [DataTestMethod]
+        [DataRow(@"{'driver': 'chrome', 'keyword':'Alexander', 'application':'https://gravitymvctestapplication.azurewebsites.net/Student'}")]
+        public void SearchStudentUiTest(string testParams)
         {
-            var actual = new SearchStudents().Execute().Actual;
+            // generate test params
+            var parameters = JsonConvert.DeserializeObject<Dictionary<string, object>>(testParams);
             
+            // execute with parameters
+            var actual = new SearchStudents()
+                .WithTestParams(parameters)
+                .Execute().Actual;
+            
+            // assert result
             Assert.IsTrue(actual);
         }
     }
